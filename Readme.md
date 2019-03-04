fa4ff18a132a3fcb5a832d3786f388568afb6fb8
Aster <galaster@foxmail.com>
2019-03-04 11:56:37 +0800

    修复::诗经章节路由错误🐛

--- Readme.md
+++ Readme.md
@@ -4,3 +4,5 @@ https://ctext.org/zh
 https://ctext.org/tools/api
 
 https://ctext.org/plugins/apilist/
+
+访问频率每秒一次, 如果触发上限则过一分钟重试
--- 詩經/main.m
+++ 詩經/main.m
@@ -63,7 +63,7 @@ Block[
 	];
 	chapters = Flatten@{
 		chapters,
-		format["book-of-poetry/sacrificial-odes-of-zhou/zh", "\:980c", {
+		format["book-of-poetry/odes-of-the-temple-and-the-altar/zh", "\:980c", {
 			XMLElement["a", {__, "href" -> "book-of-poetry/praise-odes-of-lu/zh"}, {"\:9b6f\:980c"}],
 			XMLElement["a", {__, "href" -> "book-of-poetry/sacrificial-odes-of-shang/zh"}, {"\:5546\:980c"}]
 		}]
@@ -77,23 +77,22 @@ Block[
 
 
 Block[
-	{$wait = 0.5, askS, askT, read},
+	{$wait = 10, chapters, askS, askT, read},
 	If[FileExistsQ@"data.json", Return[Nothing]];
-	askS[url_String] := Check[
-		Pause@RandomReal[$wait];
-		Import["https://api.ctext.org/gettext?if=zh&remap=gb&urn=" <> url, "RawJSON"],
-		askS[url]
+	chapters = Normal@Import["Chapter.CSV", {"CSV", "Dataset"}, "HeaderLines" -> 1];
+	askS[url_String] := Block[
+		{ask = Import["https://api.ctext.org/gettext?if=zh&remap=gb&urn=" <> url, "RawJSON"]},
+		If[!ListQ@ask["fulltext"], Pause@RandomReal[$wait];askS[url], ask]
 	];
-	askT[url_String] := Check[
-		Pause@RandomReal[$wait];
-		Import["https://api.ctext.org/gettext?if=zh&urn=" <> url, "RawJSON"],
-		askT[url]
+	askT[url_String] := Block[
+		{ask = Import["https://api.ctext.org/gettext?if=zh&urn=" <> url, "RawJSON"]},
+		If[!ListQ@ask["fulltext"], Pause@RandomReal[$wait];askS[url], ask]
 	];
 	read[record_Association] := <|
 		"Chapter" -> record@"Chapter",
 		"Traditional" -> askT[record@"Token"]["fulltext"],
 		"Simplified" -> askS[record@"Token"]["fulltext"]
 	|>;
-	data = MapMonitor[read, chapters][[2]];
+	data = Flatten@MapMonitor[read, chapters][[2]];
 	Export["data.json", Dataset@data, "RawJSON"]
 ];
