1b5bdfbdc4937bc6e606bf0a25eed992ad9745b9
Aster <galaster@foxmail.com>
2019-03-07 00:37:53 +0800

    模块::荀子章节分析📲

---
+++ 荀子/main.m
@@ -0,0 +1,59 @@
+(* ::Package:: *)
+
+(* ::Chapter:: *)
+(*Settings*)
+
+
+SetDirectory@NotebookDirectory[];
+
+
+(* ::Chapter:: *)
+(*Chapters*)
+
+
+Block[
+	{format, $tasks, name = {"\:8340\:5b50", "xunzi"}},
+	If[FileExistsQ@"Chapter.CSV", Return[Nothing]];
+	format[h_, c_] := If[
+		StringStartsQ[h, Last@name] && StringEndsQ[h, "zh"],
+		<|
+			"Chapter" -> StringJoin[First@name, "|" , c],
+			"Routing" -> StringTake[h, ;; -4],
+			"Token" -> "ctp:" <> StringTake[h, ;; -4]
+		|>,
+		Nothing
+	];
+	$tasks = Cases[
+		Import["https://ctext.org/" <> Last@name <> "/zh", {"HTML", "XMLObject"}],
+		XMLElement["a", {"shape" -> "rect", "href" -> h_}, {c_}] :> format[h, c],
+		Infinity
+	];
+	Export["Chapter.CSV", Dataset@$tasks]
+];
+
+
+(* ::Chapter:: *)
+(*Content*)
+
+
+MapMonitor = ResourceFunction["DynamicMap"];
+Block[
+	{$wait = 10, chapters, askS, askT, read},
+	If[FileExistsQ@"data.json", Return[Nothing]];
+	chapters = Normal@Import["Chapter.CSV", {"CSV", "Dataset"}, "HeaderLines" -> 1];
+	askS[url_String] := Block[
+		{ask = Import["https://api.ctext.org/gettext?if=zh&remap=gb&urn=" <> url, "RawJSON"]},
+		If[!ListQ@ask["fulltext"], Pause@RandomReal[$wait];askS[url], ask]
+	];
+	askT[url_String] := Block[
+		{ask = Import["https://api.ctext.org/gettext?if=zh&urn=" <> url, "RawJSON"]},
+		If[!ListQ@ask["fulltext"], Pause@RandomReal[$wait];askS[url], ask]
+	];
+	read[record_Association] := <|
+		"Chapter" -> record@"Chapter",
+		"Traditional" -> askT[record@"Token"]["fulltext"],
+		"Simplified" -> askS[record@"Token"]["fulltext"]
+	|>;
+	data = MapMonitor[read, chapters][[2]];
+	Export["data.json", Flatten@data, "RawJSON"]
+];
