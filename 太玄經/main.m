e2c5a2d772b689563893c4040e6b2534f54bc2bf
Aster <galaster@foxmail.com>
2019-03-19 15:14:42 +0800

    模块::太玄经章节分析📲

---
+++ 太玄經/main.m
@@ -0,0 +1,72 @@
+(* ::Package:: *)
+
+(* ::Chapter:: *)
+(*Settings*)
+
+
+SetDirectory@NotebookDirectory[];
+
+
+(* ::Chapter:: *)
+(*Auxiliary Functions*)
+
+
+<< NETLink`;
+InstallNET[];
+LoadNETType["System.Text.Encoding"];
+MapMonitor = ResourceFunction["DynamicMap"];
+
+
+(* ::Chapter:: *)
+(*Chapters*)
+
+
+Block[
+	{format, $tasks, name = {"\:592a\:7384\:7d93", "taixuanjing"}},
+	If[FileExistsQ@"Chapter.CSV", Return[Nothing]];
+	format[h_, c_] := If[
+		StringStartsQ[h, Last@name] && StringEndsQ[h, "zh"],
+		<|
+			"Chapter" -> StringJoin[First@name, "|" , c],
+			"Routing" -> StringTake[h, ;; -4],
+			"Token" -> "ctp:" <> StringTake[h, ;; -4]
+		|>,
+		Nothing
+	];
+	$tasks = Cases[
+		Import["https://ctext.org/" <> Last@name <> "/zh", {"HTML", "XMLObject"}],
+		XMLElement["a", {"shape" -> "rect", "href" -> h_}, {c_}] :> format[h, c],
+		Infinity
+	];
+	Export["Chapter.CSV", Dataset@$tasks]
+];
+
+
+(* ::Chapter:: *)
+(*Content*)
+
+
+Block[
+	{$wait = 10, reader, chapters, ask, json, askS, askT, read},
+	If[FileExistsQ@"data.json", Return[Nothing]];
+	reader = Encoding`UTF8;
+	json = ImportString[FromCharacterCode@ToCharacterCode[#, "UTF-8"], "RawJSON"]&;
+	chapters = Normal@Import["Chapter.CSV", {"CSV", "Dataset"}, "HeaderLines" -> 1];
+	askS[url_String] := Block[
+		{link = "https://api.ctext.org/gettext?if=zh&remap=gb&urn=" <> url},
+		ask = json@reader@GetString[Normal@URLRead[link, "BodyByteArray"]];
+		If[!ListQ@ask["fulltext"], Pause@RandomReal[$wait];askS[url], ask]
+	];
+	askT[url_String] := Block[
+		{link = "https://api.ctext.org/gettext?if=zh&urn=" <> url},
+		ask = json@reader@GetString[Normal@URLRead[link, "BodyByteArray"]];
+		If[!ListQ@ask["fulltext"], Pause@RandomReal[$wait];askT[url], ask]
+	];
+	read[record_Association] := <|
+		"Chapter" -> record@"Chapter",
+		"Traditional" -> askT[record@"Token"]["fulltext"],
+		"Simplified" -> askS[record@"Token"]["fulltext"]
+	|>;
+	data = MapMonitor[read, chapters][[2]];
+	Export["data.json", Flatten@data, "RawJSON"]
+];
