fa747554b3144499db855e23b859d9bae294a52f
Aster <galaster@foxmail.com>
2019-03-19 15:43:14 +0800

    校对::修复山海经 Unicode 扩展字符🗃

--- 山海經/main.m
+++ 山海經/main.m
@@ -11,13 +11,11 @@ SetDirectory@NotebookDirectory[];
 (*Auxiliary Functions*)
 
 
-<< NETLink`;
-InstallNET[];
-LoadNETType["System.Text.Encoding"];
+$names={"\:5c71\:6d77\:7ecf","\:5c71\:6d77\:7d93", "shan-hai-jing"}
 MapMonitor = ResourceFunction["DynamicMap"];
 
 
-(* ::Chapter:: *)
+(* ::Chapter::Closed:: *)
 (*Chapters*)
 
 
@@ -42,31 +40,80 @@ Block[
 ];
 
 
-(* ::Chapter:: *)
+(* ::Chapter::Closed:: *)
 (*Content*)
 
 
 Block[
-	{$wait = 10, reader, chapters, ask, json, askS, askT, read},
+	{$wait = 10, chapters, askS, askT, read},
 	If[FileExistsQ@"data.json", Return[Nothing]];
-	reader = Encoding`UTF8;
-	json = ImportString[FromCharacterCode@ToCharacterCode[#, "UTF-8"], "RawJSON"]&;
 	chapters = Normal@Import["Chapter.CSV", {"CSV", "Dataset"}, "HeaderLines" -> 1];
 	askS[url_String] := Block[
-		{link = "https://api.ctext.org/gettext?if=zh&remap=gb&urn=" <> url},
-		ask = json@reader@GetString[Normal@URLRead[link, "BodyByteArray"]];
+		{ask = Import["https://api.ctext.org/gettext?if=zh&remap=gb&urn=" <> url, "RawJSON"]},
 		If[!ListQ@ask["fulltext"], Pause@RandomReal[$wait];askS[url], ask]
 	];
 	askT[url_String] := Block[
-		{link = "https://api.ctext.org/gettext?if=zh&urn=" <> url},
-		ask = json@reader@GetString[Normal@URLRead[link, "BodyByteArray"]];
-		If[!ListQ@ask["fulltext"], Pause@RandomReal[$wait];askT[url], ask]
+		{ask = Import["https://api.ctext.org/gettext?if=zh&urn=" <> url, "RawJSON"]},
+		If[!ListQ@ask["fulltext"], Pause@RandomReal[$wait];askS[url], ask]
 	];
-	read[record_Association] := <|
-		"Chapter" -> record@"Chapter",
-		"Traditional" -> askT[record@"Token"]["fulltext"],
-		"Simplified" -> askS[record@"Token"]["fulltext"]
-	|>;
+	read[record_Association] := (
+		PrintTemporary[record@"Chapter"];
+		<|
+			"Chapter" -> record@"Chapter",
+			"Traditional" -> askT[record@"Token"]["fulltext"],
+			"Simplified" -> askS[record@"Token"]["fulltext"]
+		|>
+	);
 	data = MapMonitor[read, chapters][[2]];
 	Export["data.json", Flatten@data, "RawJSON"]
+];
+
+
+(* ::Chapter:: *)
+(*Summary*)
+
+
+Block[
+	{i = 0.9, json, data, text, rank},
+	data = Import["data.json", "RawJSON"];
+	text = StringDelete[
+		StringJoin@Flatten[#["Traditional"]& /@ data],
+		StringPartition["\:ff0c\:3001\:3002\:ff1f\:ff01\:ff1b\:ff1a\:300c\:300d\:300e\:300f\:300a\:300b", 1]
+	];
+	rank = Reverse@SortBy[Tally@StringSplit[text, ""], Last];
+	If[
+		FileExistsQ@"WordCloud.png",
+		Nothing,
+		yun = WordCloud[
+			(#2 + RandomReal[{-i, 1 / (1 - i)}]) -> #1& @@@ rank,
+			PlotTheme -> "Monochrome", ImageSize -> {16, 9} * 80, FontFamily -> "\:6977\:4f53", AspectRatio -> 9 / 16
+		];
+		Export["WordCloud.png", yun, Background -> None]
+	];
+	If[
+		FileExistsQ@"WordFrequency.png",
+		Nothing,
+		bar = RectangleChart[
+			Association[Style[#1, FontFamily -> "\:6977\:4f53", FontSize -> 14] -> {1, #2}& @@@ Reverse@Take[rank, UpTo@25]],
+			BarOrigin -> Left, ChartLabels -> Automatic, BarSpacing -> None,
+			TargetUnits -> {"Minutes", "Feet"}, AxesLabel -> Automatic, Ticks -> True, PlotTheme -> "Detailed",
+			PerformanceGoal -> "Speed", LabelingFunction -> (Placed[Last@#, Right]&),
+			ColorFunction -> Function[{width, height}, ColorData["TemperatureMap"][height]],
+			ImageSize -> {16, 9} * 80, AspectRatio -> 9 / 16
+		];
+		Export["WordFrequency.png", bar, Background -> None]
+	];
+	readme = StringRiffle[
+		{
+			"# ["<>$names[[1]]<>"](https://ctext.org/"<>$names[[3]]<>"/zh)",
+			badge[<|
+				"Chapter" -> Length@data,
+				"Character" -> StringLength@StringJoin@Flatten[#["Traditional"]& /@ data],
+				"Zipf" -> N[s /. FindDistributionParameters[Last /@ rank, ZipfDistribution[s]], 6]
+			|>],
+			"![WordCloud](https://github.com/GalAster/api.ctext.org/blob/master/"<>$names[[2]]<>"/WordCloud.png?raw=true)"
+		},
+		"\n"
+	];
+	Export["Readme.md", readme,"Text"]
 ];
